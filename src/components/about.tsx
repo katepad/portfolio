78dc8a90fd6f135ad73e75629b940d1d6a5462b8
katepad <katepad2020@gmail.com>
2025-05-27 22:06:35 -0700

    edit about section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import '../styles/global.css'; 
+import '../styles/about.css';
 
 const About: React.FC = () => {
   return (
-    <h5>About</h5>
+    <>
+    <section className='about-section section-lightblue'>
+      <div className='about-me-div section-darkblue'>
+        <h1 className = 'page-h1 color-white'> About Me </h1>
+        <hr/>
+        <p className='page-p color-lightblue'> blah</p>
+      </div>
+    </section>
+    </>
   );
 };
 
